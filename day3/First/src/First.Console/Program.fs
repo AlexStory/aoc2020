af0702b57fda6e5c1b5a70ca3bde50aacd50f2f6
alex story <astory11@yahoo.com>
2020-12-09 13:16:13 -0600

    did second day 2

--- day3/First/src/First.Console/Program.fs
+++ day3/First/src/First.Console/Program.fs
@@ -9,15 +9,14 @@ module Console =
     | '.' -> Plain
     | _ -> failwith "Invalid Argument"
 
-    let content = File.ReadAllLines "assets/input.txt"
+    let content = File.ReadAllLines (__SOURCE_DIRECTORY__ + "../../../assets/input.txt")
     let x, y = 0, 0
-    let xStep = 3
-    let yStep = 1
     let height = Array.length content
     let trackWidth = content |> Array.head |> String.length
     let rebalanceX x = x % trackWidth
 
-    let rec count x y trees =
+
+    let rec count x y xStep yStep trees =
         let xTarget = rebalanceX (x + xStep)
         let yTarget = y + yStep
 
@@ -25,11 +24,23 @@ module Console =
             trees
         else
             match getSpot content.[yTarget].[xTarget] with
-            | Tree -> count xTarget yTarget (trees + 1)
-            | Plain -> count xTarget yTarget trees
+            | Tree -> count xTarget yTarget xStep yStep (trees + 1)
+            | Plain -> count xTarget yTarget xStep yStep trees
+
+    let startCount xStep yStep = count 0 0 xStep yStep 0
 
     [<EntryPoint>]
     let main argv =
-        count 0 0 0
-        |> printfn "%A"
+        let oneOne =  startCount 1 1 
+        let threeOne = startCount 3 1 
+        let fiveOne = startCount 5 1 
+        let sevenOne = startCount 7 1
+        let oneTwo = startCount 1 2
+        
+        printfn "1-1 %d" oneOne
+        printfn "3-1 %d" threeOne
+        printfn "5-1 %d" fiveOne
+        printfn "7-1 %d" sevenOne
+        printfn "1-2 %d" oneTwo
+        printfn "total %A" (bigint oneOne * bigint threeOne * bigint fiveOne * bigint sevenOne * bigint oneTwo)
         0 // return an integer exit code
