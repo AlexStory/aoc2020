e97b0e174009e559665239e73dc61ec54f2c94d2
alex story <astory11@yahoo.com>
2020-12-11 10:27:49 -0600

    day 10 part 2

--- day10/Jolter/Jolter.Console/Program.fs
+++ day10/Jolter/Jolter.Console/Program.fs
@@ -13,16 +13,35 @@ let content =
     __SOURCE_DIRECTORY__ + "/input.txt"
     |> File.ReadAllLines
     |> Seq.map int
+    |> Seq.sort
 
 let differences = 
     content
-    |> Seq.sort
     |> Seq.fold getJolt (0, 0, 0)
 
+let canReach original target = 
+    target > original 
+    && target - original <= 3
+
+
+let rec pathCount lst =
+    printfn "%A" lst
+    match lst with
+    | [_, count] -> count
+    | (hn, hc) :: tail ->
+        pathCount (List.map (fun (n, c) -> (n, if canReach hn n then hc + c else c)) tail)
+
+
+
+// let part1 = 
+//     differences
+//     |> fun (a, b, c) -> printfn "joltages: %d, ones: %d, threes: %d, answer: %d" a b c (b * (c + 1))
 
 
 [<EntryPoint>]
 let main argv =
-    differences
-    |> fun (a, b, c) -> printfn "joltages: %d, ones: %d, threes: %d, answer: %d" a b c (b * (c + 1))
+    let counts = (0, bigint 1) :: List.map (fun x -> x, bigint 0) (Seq.toList content)
+    counts
+    |> pathCount
+    |> printfn "%A"
     0 // return an integer exit code
